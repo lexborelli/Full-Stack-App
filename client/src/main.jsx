c8fe068d5a9fd7c64d0bdcfe7b765ef3b31a38fc
lexborelli <alexasborelli@gmail.com>
2024-12-04 22:34:15 -0500

    deleted browserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react"; 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 //imported css styles files
 import './styles/reset.css';
 import './styles/global.css';
@@ -12,10 +11,8 @@ import { UserProvider } from "./context/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
